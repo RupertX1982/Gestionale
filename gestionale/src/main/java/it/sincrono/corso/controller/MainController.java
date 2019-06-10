f6c78ca86129309539de29d614f9e557b44bfa39
Utente <Utente@DESKTOP-MJC5OMH>
2019-06-10 16:33:06 +0200

    scczc

---
+++ gestionale/src/main/java/it/sincrono/corso/controller/MainController.java
@@ -0,0 +1,25 @@
+package it.sincrono.corso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+
+public class MainController {
+
+@RequestMapping("index")
+	
+	public String getIndex(Model m) {
+		
+		return "index";
+	}
+	
+}
+
+
+
+
+
+
